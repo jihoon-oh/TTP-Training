e621b3c3547cc0fb27a4ef2416de66334f37d88b
Jihoon Oh <jihoonoh123@gmail.com>
2016-01-18 22:21:33 -0500

    digital clock

---
+++ Digital Clock/main.js
@@ -0,0 +1,36 @@
+setInterval(function() {
+	var d = new Date();
+	var h = d.getHours();
+	var m = d.getMinutes();
+	var s = d.getSeconds();
+	var ampm;
+
+	if(h>=12) {
+		ampm = "PM";
+	} else if(h<12 || h === 24) {
+		ampm = "AM"
+	}
+	if(h > 12) {
+		h = h - 12;
+	}
+	if(h<10) {
+		h = "0" + h;
+	}
+	if(m<10) {
+		m = "0" + m;
+	}
+	if(s<10) {
+		s = "0" + s;
+	}
+	document.getElementById("hour").innerHTML =  h;
+	document.getElementById("minute").innerHTML = m;
+	document.getElementById("second").innerHTML = s;
+	document.getElementById("ampm").innerHTML = ampm;
+}, 1000);
+
+setInterval(function() {
+	var d = new Date();
+	var monthArray = [January, February, March, April, June, July, August, September, October, November, December];
+	var month = monthArray[d.getMonth()];
+
+}, 100000)
